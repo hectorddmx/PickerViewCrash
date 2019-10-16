ffddaacbd651fddd5a53b136804cc139ac43efa8
Hector De Diego <h.dediego@distillery.com>
2019-10-16 10:34:41 -0500

    Initial Commit

---
+++ TestingAgain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingAgain
+//
+//  Created by Hector De Diego on 16/10/19.
+//  Copyright © 2019 dediego. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
